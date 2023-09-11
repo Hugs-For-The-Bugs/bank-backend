e227c5c512d24f8e356396cdc2f787e3123e0f80
Lambo77 <Lambozhuang@icloud.com>
2023-09-11 20:55:31 +0200

    added Gin Framwork and test api

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello")
+	router := gin.Default()
+
+	// test api
+	router.GET("/test", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello, World!",
+		})
+	})
+
+	router.Run("localhost:8080")
 }
