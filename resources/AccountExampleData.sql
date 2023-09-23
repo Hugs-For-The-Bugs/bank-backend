2991b75d6cd32b0a687af08efbb92363e0cb46bf
Ramin Jafari <93491925+Ramin-JFR@users.noreply.github.com>
2023-09-23 18:17:30 +0200

    Remove the old sql files

---
+++ resources/AccountExampleData.sql
@@ -0,0 +1,23 @@
+USE bank;
+
+INSERT INTO accounts  (id, first_name, surname, birthday, phone, email, social_security_number, password, balance) VALUES
+(11, 'Alice', 'Smith', '19900101', '123-456-7890', 'alice@email.com', '199001015678', '7c6a180b36896a0a8c02787eeafb0e4c', 1000),
+(12, 'Bob', 'Johnson', '19850215', '987-654-3210', 'bob@email.com', '198502157432', '6cb75f652a9b52798eb6cf2201057c73', 2000),
+(13, 'Carol', 'Williams', '19920320', '555-444-3333', 'carol@email.com', '199203203344', '819b0643d6b89dc9b579fdfc9094f28e', 3000),
+(14, 'Dave', 'Brown', '19880412', '111-222-3333', 'dave@email.com', '198804121233', '34cc93ece0ba9e3f6f235d4af979b16c', 4000),
+(15, 'Emily', 'Jones', '19950525', '666-555-4444', 'emily@email.com', '199505255444', 'db0edd04aaac4506f7edab03ac855d56', 5000),
+(16, 'Frank', 'Davis', '19790618', '777-666-5555', 'frank@email.com', '197906185555', '218dd27aebeccecae69ad8408d9a36bf', 6000),
+(17, 'Grace', 'Miller', '19830722', '888-777-6666', 'grace@email.com', '198307226666', '00cdb7bb942cf6b290ceb97d6aca64a3', 7000),
+(18, 'Henry', 'Wilson', '19980811', '999-888-7777', 'henry@email.com', '199808117777', 'b25ef06be3b6948c0bc431da46c2c738', 8000),
+(19, 'Isabel', 'Moore', '20010909', '101-010-1010', 'isabel@email.com', '200109091010', '5d69dd95ac183c9643780ed7027d128a', 9000),
+(20, 'Jack', 'Taylor', '19751020', '202-020-2020', 'jack@email.com', '197510202020', '7c6a180b36896a0a8c02787eeafb0e4c0', 10000),
+(21, 'Karen', 'Anderson', '19801115', '303-030-3030', 'karen@email.com', '198011153030', '7c6a180b36896a0a8c02787eeafb0e4c1', 11000),
+(22, 'Leo', 'Thomas', '19911205', '404-040-4040', 'leo@email.com', '199112054040', '7c6a180b36896a0a8c02787eeafb0e4c2', 12000),
+(23, 'Mia', 'Jackson', '19840130', '505-050-5050', 'mia@email.com', '198401305050', '7c6a180b36896a0a8c02787eeafb0e4c3', 13000),
+(24, 'Nate', 'White', '19990224', '606-060-6060', 'nate@email.com', '199902246060', '7c6a180b36896a0a8c02787eeafb0e4c4', 14000),
+(25, 'Olivia', 'Harris', '20000315', '707-070-7070', 'olivia@email.com', '200003157070', '7c6a180b36896a0a8c02787eeafb0e4c5', 15000),
+(26, 'Paul', 'Clark', '19820428', '808-080-8080', 'paul@email.com', '198204288080', '7c6a180b36896a0a8c02787eeafb0e4c6', 16000),
+(27, 'Quinn', 'Lewis', '19940510', '909-090-9090', 'quinn@email.com', '199405109090', '7c6a180b36896a0a8c02787eeafb0e4c7', 17000),
+(28, 'Ryan', 'Scott', '19780622', '010-101-0101', 'ryan@email.com', '197806220101', '7c6a180b36896a0a8c02787eeafb0e4c8', 18000),
+(29, 'Sophia', 'Walker', '19970715', '121-212-1212', 'sophia@email.com', '199707151212', '7c6a180b36896a0a8c02787eeafb0e4c9', 19000),
+(30, 'Tom', 'Hall', '19650831', '232-323-2323', 'tom@email.com', '196508313232', '6cb75f652a9b52798eb6cf2201057c730', 20000);
