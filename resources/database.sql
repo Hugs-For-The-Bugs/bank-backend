4791a34dc725a7efb4637a9f0b2c85b1ea376a59
PDI7 <patrik.messerli@gmx.ch>
2023-09-25 15:23:53 +0200

    hash passwords of mock data

--- resources/database.sql
+++ resources/database.sql
@@ -39,11 +39,11 @@ CREATE TABLE IF NOT EXISTS admins (
 
 -- Insert data into account table
 INSERT INTO accounts (first_name, surname, birthday, phone, email, ssn, password, balance, active) VALUES
-('John', 'Doe', '19900101', '1234567890', 'john@email.com', '199001011234', 'password1', '1000', true),
-('Jane', 'Doe', '19910202', '0987654321', 'jane@email.com', '199102022345', 'password2', '2000', true),
-('Alice', 'Smith', '19850303', '1122334455', 'alice@email.com', '198503033456', 'password3', '3000', false),
-('Bob', 'Johnson', '19800404', '2233445566', 'bob@email.com', '198004044567', 'password4', '4000', true),
-('Tom', 'Cruise', '19620703', '9191919191', 'tom@email.com', '196207035678', 'password5', '5000', true);
+('John', 'Doe', '19900101', '1234567890', 'john@email.com', '199001011234', '7c6a180b36896a0a8c02787eeafb0e4c', '1000', true), -- password1
+('Jane', 'Doe', '19910202', '0987654321', 'jane@email.com', '199102022345', '6cb75f652a9b52798eb6cf2201057c73', '2000', true), -- password2
+('Alice', 'Smith', '19850303', '1122334455', 'alice@email.com', '198503033456', '819b0643d6b89dc9b579fdfc9094f28e', '3000', false), -- password3
+('Bob', 'Johnson', '19800404', '2233445566', 'bob@email.com', '198004044567', '34cc93ece0ba9e3f6f235d4af979b16c', '4000', true), -- password4
+('Tom', 'Cruise', '19620703', '9191919191', 'tom@email.com', '196207035678', 'db0edd04aaac4506f7edab03ac855d56', '5000', true); -- password5
 
 -- Insert data into transaction table
 INSERT INTO transactions (from_account_id, to_account_id, amount, type, state) VALUES
@@ -54,4 +54,4 @@ INSERT INTO transactions (from_account_id, to_account_id, amount, type, state) V
 (5, 1, '400', 'Transfer', 'Successful');
 
 INSERT INTO admins (username, password) VALUES
-('admin', 'admin');
+('admin', '21232f297a57a5a743894a0e4a801fc3'); -- admin
