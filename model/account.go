e36cb1046cd356922280c092bdb43520f61ee521
Lambo77 <Lambozhuang@icloud.com>
2023-10-02 13:55:38 +0200

    change *int to int

--- model/account.go
+++ model/account.go
@@ -9,6 +9,6 @@ type Account struct {
 	Phone                string `json:"phone"`
 	SocialSecurityNumber string `json:"socialSecurityNumber"`
 	Surname              string `json:"surname"`
-	Balance              *int   `gorm:"default:0" json:"balance"`
+	Balance              int    `gorm:"default:0" json:"balance"`
 	Active               bool   `gorm:"default:false" json:"active"`
 }
