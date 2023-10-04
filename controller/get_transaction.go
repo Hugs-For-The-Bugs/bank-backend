a0e9bbfeced50f16d27002c3f6c092ede74aa882
Ramin Jafari <93491925+Ramin-JFR@users.noreply.github.com>
2023-10-04 13:43:41 +0200

    removing the balance and password

--- controller/get_transaction.go
+++ controller/get_transaction.go
@@ -25,7 +25,7 @@ func GetTransactions(c *gin.Context) {
 	id := session.Get("id")
 
 	result := util.DB.Raw(`
-SELECT transactions.*, fromAccounts.social_security_number as from_social_security_number, fromAccounts.id, toAccounts.social_security_number as to_social_security_number, fromAccounts.phone as FromPhone,  toAccounts.phone as ToPhone , fromAccounts.balance as BalanceForSender,  toAccounts.balance AS BalanceForReceiver
+SELECT transactions.*, fromAccounts.social_security_number as from_social_security_number, fromAccounts.id, toAccounts.social_security_number as to_social_security_number, fromAccounts.phone as FromPhone,  toAccounts.phone as ToPhone 
 	FROM transactions
 	JOIN accounts AS fromAccounts ON fromAccounts.id = transactions.from_account_id
 	JOIN accounts AS toAccounts ON toAccounts.id = transactions.to_account_id
